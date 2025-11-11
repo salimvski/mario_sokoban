11412f3f9398532a48851ef398345a6640d88a46
salimvski <salim.elrhilani@gmail.com>
2025-11-11 00:45:47 -0500

    clean code

--- src/render.h
+++ src/render.h
@@ -4,13 +4,10 @@
 #include <SDL2/SDL.h>
 #include "level.h"
 
-// Initialize textures once after renderer creation
 void init_textures(SDL_Renderer *renderer);
 
-// Free all textures when closing the game
 void destroy_textures();
 
-// Render the entire map + player
 void render_map_sdl(SDL_Renderer *renderer, Level *lvl);
 
 #endif
