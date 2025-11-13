7ae9001da029c8649d0ed31bde1497ffb43210cc
salimvski <salim.elrhilani@gmail.com>
2025-11-12 23:27:52 -0500

    added logic to handle custom commands

--- src/server.h
+++ src/server.h
@@ -1,6 +1,10 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <sys/types.h>  // for ssize_t
+#include <stddef.h>     // for size_t
+#include <unistd.h>
+
 typedef struct {
     int sock;
     int id;
@@ -8,7 +12,7 @@ typedef struct {
 } client_info_t;
 
 
-ssize_t read_n_bytes(int sock, void *buf, size_t n);
+ssize_t read_n_bytes(int sock, void* buf, size_t n);
 
 void handle_sigint(int sig);
 
